74cd7950290c5af6810a5f2f497b99270f19b8cd
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-14 20:06:56 +0530

    controller done with strange time complexity 50sec

--- src/controllers/coinController.js
+++ src/controllers/coinController.js
@@ -1,8 +1,43 @@
-const CoinModel = require("../models/coinModel");
+const coinModel = require("../models/coinModel");
+const axios = require("axios");
 
 exports.getCoins = async (req, res) => {
   try {
+    let assets = await axios.get("https://api.coincap.io/v2/assets");
+
+    let coinData = assets.data;
+
+    let checkData = await coinModel.find();
+
+    if (checkData.length === 0) {
+      let arr = [];
+
+      for (let i = 0; i < coinData.data.length; i++) {
+        let newDoc = {
+          symbol: coinData.data[i].symbol,
+          name: coinData.data[i].name,
+          marketCapUsd: coinData.data[i].marketCapUsd,
+          priceUsd: coinData.data[i].priceUsd,
+        };
+
+        let createData = await coinModel.create(newDoc);
+
+        arr.push(newDoc);
+      }
+      return res.send({ status: true, msg: "Coin data is created" });
+    } else {
+      let getData = await coinModel.find();
+
+      for (let i = 0; i < getData.length; i++) {
+        getData[i].changePercent24Hr = coinData.data[i].changePercent24Hr;
+      }
+
+      for (let i = 0; i < getData.length; i++) {
+        getData.sort((x, y) => x.changePercent24Hr - y.changePercent24Hr);
+      }
+      return res.status(200).send({ status: true, data: getData });
+    }
   } catch (err) {
-    console.log(err.message);
+    return res.status(500).send({ status: false, msg: err.message });
   }
 };
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const coinController = require("../controllers/coinController");
 
-router.get("/getCoins", coinController.getCoins);
+router.get("/assets", coinController.getCoins);
 
 module.exports = router;
