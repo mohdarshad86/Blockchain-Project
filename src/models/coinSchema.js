57ec66d260d88b8f9329d7e1e56402c114bce04c
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-14 17:40:29 +0530

    Schema created

---
+++ src/models/coinSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const coinSchema = new mongoose.Schema({
+  symbol :{
+    type :String  ,
+    unique: true
+  },
+  name : {
+    type :String  ,
+    unique: true
+  },
+  marketCapUsd : String,
+  priceUsd : String
+})
+
+module.exports = mongoose.model("Coin", coinSchema)
+
+// {  "symbol" // String and Unique
+//     "name": // String and Unique
+//     "marketCapUsd": // String  ( not Number)
+//      "priceUsd": //String
+//    }
